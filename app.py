7a67d37ba77de38d3557b042a90da0cf1965b7a9
zee-078 <m.zeeshan.haider009@gmail.com>
2025-07-13 21:31:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    # Pass static URLs to the template for use in JavaScript and CSS
+    image_urls = {
+        "DJI_0633": url_for('static', filename='images/DJI_0633.JPG'),
+        "img": url_for('static', filename='images/img.jpg'),
+        "img2": url_for('static', filename='images/img2.jpg'),
+        "fort_york": url_for('static', filename='images/fort_york.jpg'),
+        "cn_tower_pdf": url_for('static', filename='docs/CN_Tower.pdf'),
+        "background_image": url_for('static', filename='images/background_image.jpg')
+    }
+    return render_template("index.html", image_urls=image_urls)
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=10000)
